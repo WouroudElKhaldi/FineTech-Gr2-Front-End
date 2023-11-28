e8f247e87dd62550f2ca9dda4eda29321936b056
WouroudElKhaldi <wouroudelkhaldi@gmail.com>
2023-11-28 20:12:59 +0200

    added the logo photo

---
+++ finetech/src/Pages/Users/Users.js
@@ -0,0 +1,10 @@
+import styles from './Users.module.css'
+
+const UserPage = () => {
+    return (
+        <div className={styles.Container}>
+            
+        </div>
+    )
+}
+export default UserPage ;
