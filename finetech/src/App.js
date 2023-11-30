8a10f0158875a8e877ff58fbcd013d22b9ef69e9
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-12-01 00:44:59 +0200

    abed_Sidebar component and layout trial

--- finetech/src/App.js
+++ finetech/src/App.js
@@ -1,10 +1,11 @@
 import './App.css'
 import Login from './Pages/Login/Login.js'
+import Sidebar from './Layouts/Sidebar/Sidebar.js';
 
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Sidebar />
     </div>
   );
 }
