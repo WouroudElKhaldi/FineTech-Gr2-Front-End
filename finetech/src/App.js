b55d6440cd3c6b09153edb1d730e9d768572c65f
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-12-01 13:07:48 +0200

    abed_Update App.js

--- finetech/src/App.js
+++ finetech/src/App.js
@@ -1,14 +1,15 @@
-import './App.css'
-import Login from './Pages/Login/Login.js'
-import Sidebar from './Layouts/Sidebar/Sidebar.js';
-import EditCompanyForm from './Components/EditCompany/EditCompanyForm.js';
+import React from "react";
 
-function App() {
+import "./App.css";
+import AppRoutes from "./Routes/AppRoutes";
+// import Login from './Pages/Login/Login.js'
+// import Sidebar from './Layouts/Sidebar/Sidebar.js';
+
+export default function App() {
   return (
     <div className="App">
-      <EditCompanyForm />
+      <AppRoutes />
+    
     </div>
   );
 }
-
-export default App;
