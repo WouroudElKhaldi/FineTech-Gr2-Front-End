295448241ef2480a8c70137457c5d047502db228
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-12-03 21:13:08 +0200

    abed_fixing layout with sidebar

--- finetech/src/Routes/LayoutWithSidebar.js
+++ finetech/src/Routes/LayoutWithSidebar.js
@@ -1,15 +1,20 @@
 import React from "react";
-import SideLayout from '../Layouts/Sidebar/Sidebar.js'
 
+import Sidebar from "../Layouts/Sidebar/Sidebar.js";
 function LayoutWithSidebar({ children }) {
   return (
     <>
       <div
         style={{
+          minHeight: "8vh",
+          margin: "0%",
+          display: "grid",
+          gridTemplateColumns: "20% 75%",
+          columnGap: "4%",
         }}
       >
-        <SideLayout/>
-        {children  }
+        <Sidebar />
+        {children}
       </div>
     </>
   );
