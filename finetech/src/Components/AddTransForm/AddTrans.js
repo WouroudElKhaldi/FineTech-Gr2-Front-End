3d268de52ce6fc00e20fe5fb05fedbc27f707828
RayanAlHassan <rayanalhassan2000@gmail.com>
2023-12-05 14:54:33 +0200

    profiledetails in progress

--- finetech/src/Components/AddTransForm/AddTrans.js
+++ finetech/src/Components/AddTransForm/AddTrans.js
@@ -1,12 +1,16 @@
-import React  from "react";
+import React from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import { Typography } from "@mui/material";
 import { Button } from "../Button/Button";
 import Stack from "@mui/material/Stack";
 import MenuItem from "@mui/material/MenuItem";
-import CloseIcon from '@mui/icons-material/Close';
+import CloseIcon from "@mui/icons-material/Close";
 import styles from "./AddTrans.module.css";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+
 ////////////////////////////////////////
 //create calender picker using mui
 const dateNow = new Date(); // Creating a new date object with the current date and time
@@ -17,18 +21,24 @@ const month = // Setting current Month number from current Date object
     ? `0${monthWithOffset}`
     : monthWithOffset;
 const date =
-dateNow.getUTCDate().toString().length < 2 // Checking if date is < 10 and pre-prending 0 if not to adjust for date input.
-? `0${dateNow.getUTCDate()}`
-: dateNow.getUTCDate();
+  dateNow.getUTCDate().toString().length < 2 // Checking if date is < 10 and pre-prending 0 if not to adjust for date input.
+    ? `0${dateNow.getUTCDate()}`
+    : dateNow.getUTCDate();
 
 const materialDateInput = `${year}-${month}-${date}`; // combining to format for defaultValue or value attribute of material <TextField>
 
 ///////////////////////////////////////
-const data = [
+const types = [
   {
-    value: "",
-    label: "None",
+    value: "Income",
+    label: "Income",
   },
+  {
+    value: "Expences",
+    label: "Expences",
+  },
+];
+const data = [
   {
     value: "Water",
     label: "Water",
@@ -49,19 +59,32 @@ const data = [
     value: "debts",
     label: "debts",
   },
-    
 ];
 
 /////////////////////////////////////////////
 
-const AddTrans = ({ handleClose }) => {
+const AddTrans = ({ handleClose, type }) => {
   const formRef = React.createRef(null); // it referance the DOM
   const [category, setCategory] = React.useState("");
+  const [selectedType, setSelectedType] = React.useState("");
   const handleChange = (event) => {
-    setCategory(event.target.value);
+    if (event.target.name === "type") {
+      setSelectedType(event.target.value);
+    } else {
+      setCategory(event.target.value);
+    }
   };
 
-  const handleFormSubmit = (e) => {
+  // const handleFormSubmit = (e) => {
+  //   e.preventDefault();
+  //   // handleClose();
+  // };
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    // handleClose();
+  };
+
+  const handleEditUser = (e) => {
     e.preventDefault();
     // handleClose();
   };
@@ -75,31 +98,31 @@ const AddTrans = ({ handleClose }) => {
     setCategory("");
   };
 
-  const divStyle ={
-    display : 'flex',
-    justifyContent: 'space-between' ,
-    width : '25rem',
-    marginTop : '1.5rem'
-}
+  const divStyle = {
+    display: "flex",
+    justifyContent: "space-between",
+    width: "25rem",
+    marginTop: "1.5rem",
+  };
 
-const spanStyle ={
-  display : 'flex', 
-  alignItems: 'center',
-}
+  const spanStyle = {
+    display: "flex",
+    alignItems: "center",
+  };
   return (
     <>
       <Box
         ref={formRef}
         component="form"
-        onSubmit={handleFormSubmit}
+        onSubmit={type === "add" ? handleAddUser : handleEditUser}
         sx={{
-          '& .MuiFormControl-root': {
-            mt: 2 , 
-            mb: 2, 
-            ml:0 , 
-            mr: 0 ,
-            width: '25rem'
-           },
+          "& .MuiFormControl-root": {
+            mt: 2,
+            mb: 2,
+            ml: 0,
+            mr: 0,
+            width: "25rem",
+          },
           "& .MuiInputBase-input": {
             color: "white",
           },
@@ -110,10 +133,10 @@ const spanStyle ={
             border: "white",
           },
         }}
-        
         autoComplete="off"
       >
         <div style={divStyle}>
+          {type === "add" ? (
             <Typography
               variant="h4"
               component="h4"
@@ -121,39 +144,70 @@ const spanStyle ={
                 textAlign: "left",
                 mt: 3,
                 mb: 3,
-                ml: '8px',
-                width: 'fit-content' ,
+                ml: "8px",
+                width: "fit-content",
                 fontWeight: "bold",
               }}
             >
               Add Transactions
             </Typography>
-            <span
-              style={spanStyle} 
-              className={styles.Exit}            
-              onClick={() => {
-                handleClose();
-                handleFromClear();
+          ) : (
+            <Typography
+              variant="h4"
+              component="h4"
+              sx={{
+                textAlign: "left",
+                mt: 3,
+                mb: 3,
+                ml: "8px",
+                width: "fit-content",
+                fontWeight: "bold",
               }}
-              >
-              <CloseIcon />
-            </span>
-          </div>
+            >
+              Edit Transactions
+            </Typography>
+          )}
 
-          <Stack spacing={2}>
-            <TextField
-              required
-              id="outlined-required"
-              label="Type"
-              placeholder="Type"
-            />
-
-            <TextField
-              id="outlined-select-currency"
-              select
-              label="Categories *"
-              defaultValue="None"
+          <span
+            style={spanStyle}
+            className={styles.Exit}
+            onClick={() => {
+              handleClose();
+              handleFromClear();
+            }}
+          >
+            <CloseIcon />
+          </span>
+        </div>
+        <Stack>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Type *</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={selectedType}
+              label="Type *"
+              onChange={(e) => handleChange(e)}
+              sx={{
+                "& .MuiSvgIcon-root": {
+                  color: (theme) => theme.palette.primary.main,
+                },
+              }}
+            >
+              {types.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Categories *</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
               value={category}
+              label="Categories *"
               onChange={(e) => handleChange(e)}
               sx={{
                 "& .MuiSvgIcon-root": {
@@ -166,55 +220,45 @@ const spanStyle ={
                   {option.label}
                 </MenuItem>
               ))}
-            </TextField>
+            </Select>
+          </FormControl>
+          <TextField
+            required
+            id="outlined-required"
+            label="Company"
+            placeholder="Company"
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="User"
+            placeholder="User"
+          />
 
-            <TextField
-              required
-              id="outlined-required"
-              label="Company"
-              placeholder="Company"
+          <TextField
+            required
+            id="outlined-required"
+            label="Amount"
+            placeholder="Amount"
+            type="number"
+          />
+          <TextField
+            required
+            id="outlined-required"
+            label="Description"
+            placeholder="Description"
+          />
+          <div style={divStyle}>
+            <Button
+              text={type === "add" ? "Add" : "Edit"}
+              color={"blue"}
+              size={"small"}
             />
-            <TextField
-              required
-              id="outlined-required"
-              label="User"
-              placeholder="User"
-            />
-            <TextField
-              required
-              id="outlined-required"
-              label="Date"
-              InputLabelProps={{ shrink: true, required: true }}
-              type="date"
-              defaultValue={materialDateInput}
-              sx={{
-                // Your other MUI styles...
-                "& input::-webkit-calendar-picker-indicator": {
-                  filter: "brightness(0) invert(1)",
-                },
-              }}
-            />
-
-            <TextField
-              required
-              id="outlined-required"
-              label="Amount"
-              placeholder="Amount"
-              type="number"
-            />
-            <TextField
-              required
-              id="outlined-required"
-              label="Description"
-              placeholder="Description"
-            />
-              <div style={divStyle}>
-                <Button text={"Submit"} color={"blue"} size={"small"} />
-                <span onClick={handleFromClear}>
-                  <Button text={"Cancel"} color={"Gray"} size={"small"} />
-                </span>
-              </div>
-          </Stack>
+            <span onClick={handleFromClear}>
+              <Button text={"Clear"} color={"Gray"} size={"small"} />
+            </span>
+          </div>
+        </Stack>
       </Box>
     </>
   );
--- finetech/src/Components/Table/Table.js
+++ finetech/src/Components/Table/Table.js
@@ -1,129 +1,168 @@
-import { useState, useEffect } from 'react';
-import { Box } from '@mui/material';
-import { DataGrid , GridToolbar} from '@mui/x-data-grid';
-import Grid from '@mui/material/Unstable_Grid2';
-import DeleteModal from '../DeleteUserForm/DeleteModal';  
-import DeleteTransModal from '../EditTransForm/DeleteTransModal';
-import UserModal from '../AddUserForm/AddUserModal';
-import TransModal from '../AddTransForm/AddTransModal';
+import { useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import Grid from "@mui/material/Unstable_Grid2";
+import DeleteModal from "../DeleteUserForm/DeleteModal";
+import DeleteTransModal from "../DeleteTransForm/DeleteTransModal";
+import UserModal from "../AddUserForm/AddUserModal";
+import TransModal from "../AddTransForm/AddTransModal";
 
-const TableComponent = ({ data , wid , isEdit , ForWhat}) => {
+const TableComponent = ({ data, wid, isEdit, ForWhat }) => {
   const [userData, setUserData] = useState(data);
   const [columns, setColumns] = useState([]);
   const [error, setError] = useState(false);
-  const buton = isEdit === true ? true : false ;
+  const buton = isEdit === true ? true : false;
 
-  useEffect(() => { 
+  useEffect(() => {
     try {
       setUserData(data);
 
       if (userData.length > 0) {
         const visibleFields = Object.keys(userData[0]);
-          if(buton === false){
-            setColumns(visibleFields.map((field) => ({ field, headerName: field , flex : 1})));
-          }else {
-            if (ForWhat === 'users' ){
-              const updatedColumns = [
-                ...visibleFields.map((field) => ({ field, headerName: field, flex: 1 })),
-                {
-                  field: 'Edit',
-                  headerName: 'Edit',
-                  renderCell: (params) => (
-                    <Grid container md={12} sx={{
-                      display: 'flex',
-                      justifyContent: "center"
-                    }}>
-                      <span>
-                        <UserModal type='edit'/>
-                      </span>
-                      </Grid>
-                  ),
-                },
-                {
-                  field: 'Delete',
-                  headerName: 'Delete',
-                  renderCell: (params) => (
-                    <Grid container md={12} sx={{
-                      display: 'flex',
-                      justifyContent: "center"
-                    }}>
-                      <span>
-                        <DeleteModal/>
-                      </span>
-                      </Grid>
-                  ),
-                }
-              ];
-              setColumns(updatedColumns)
-              }
-              else if (ForWhat === "transaction"){
-                const updatedColumns = [
-                  ...visibleFields.map((field) => ({ field, headerName: field, editable: true, flex: 1 })),
-                  {
-                    field: 'Edit',
-                    headerName: 'Edit',
-                    renderCell: (params) => (
-                      <Grid container md={12} sx={{
-                        display: 'flex',
-                        justifyContent: "center"
-                      }}>
-                        <span>
-                          <TransModal type = 'edit'/>
-                        </span>
-                        </Grid>
-                    ),
-                  },
-                  {
-                    field: 'Delete',
-                    headerName: 'Delete',
-                    renderCell: (params) => (
-                      <Grid container md={12} sx={{
-                        display: 'flex',
-                        justifyContent: "center"
-                      }}>
-                        <span>
-                          <DeleteTransModal/>
-                        </span>
-                        </Grid>
-                    ),
-                  }
-                ];
-                setColumns(updatedColumns)
-              } else {
-                const updatedColumns = [
-                  ...visibleFields.map((field) => ({ field, headerName: field, editable: true, flex: 1 })),
-                  {
-                    field: 'Edit',
-                    headerName: 'Edit',
-                    renderCell: (params) => (
-                      <Grid container md={12} sx={{
-                        display: 'flex',
-                        justifyContent: "center"
-                      }}>
-                        <span>
-                          {/* <GoalModal type='edit' /> */}
-                        </span>
-                        </Grid>
-                    ),
-                  },
-                  {
-                    field: 'Delete',
-                    headerName: 'Delete',
-                    renderCell: (params) => (
-                      <Grid container md={12} sx={{
-                        display: 'flex',
-                        justifyContent: "center"
-                      }}>
-                        <span>
-                          {/* <DeleteGoalModal/> */}
-                        </span>
-                        </Grid>
-                    ),
-                  }
-                ];
-                setColumns(updatedColumns)
-              }
-            }
+        if (buton === false) {
+          setColumns(
+            visibleFields.map((field) => ({
+              field,
+              headerName: field,
+              flex: 1,
+            }))
+          );
+        } else {
+          if (ForWhat === "users") {
+            const updatedColumns = [
+              ...visibleFields.map((field) => ({
+                field,
+                headerName: field,
+                flex: 1,
+              })),
+              {
+                field: "Edit",
+                headerName: "Edit",
+                renderCell: (params) => (
+                  <Grid
+                    container
+                    md={12}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span>
+                      <UserModal type="edit" />
+                    </span>
+                  </Grid>
+                ),
+              },
+              {
+                field: "Delete",
+                headerName: "Delete",
+                renderCell: (params) => (
+                  <Grid
+                    container
+                    md={12}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span>
+                      <DeleteModal />
+                    </span>
+                  </Grid>
+                ),
+              },
+            ];
+            setColumns(updatedColumns);
+          } else if (ForWhat === "transaction") {
+            const updatedColumns = [
+              ...visibleFields.map((field) => ({
+                field,
+                headerName: field,
+                editable: true,
+                flex: 1,
+              })),
+              {
+                field: "Edit",
+                headerName: "Edit",
+                renderCell: (params) => (
+                  <Grid
+                    container
+                    md={12}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span>
+                      <TransModal type="edit" />
+                    </span>
+                  </Grid>
+                ),
+              },
+              {
+                field: "Delete",
+                headerName: "Delete",
+                renderCell: (params) => (
+                  <Grid
+                    container
+                    md={12}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span>
+                      <DeleteTransModal />
+                    </span>
+                  </Grid>
+                ),
+              },
+            ];
+            setColumns(updatedColumns);
+          } else {
+            const updatedColumns = [
+              ...visibleFields.map((field) => ({
+                field,
+                headerName: field,
+                editable: true,
+                flex: 1,
+              })),
+              {
+                field: "Edit",
+                headerName: "Edit",
+                renderCell: (params) => (
+                  <Grid
+                    container
+                    md={12}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span>{/* <GoalModal type='edit' /> */}</span>
+                  </Grid>
+                ),
+              },
+              {
+                field: "Delete",
+                headerName: "Delete",
+                renderCell: (params) => (
+                  <Grid
+                    container
+                    md={12}
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <span>{/* <DeleteGoalModal/> */}</span>
+                  </Grid>
+                ),
+              },
+            ];
+            setColumns(updatedColumns);
+          }
+        }
       } else {
         setColumns([]);
       }
@@ -133,63 +172,75 @@ const TableComponent = ({ data , wid , isEdit , ForWhat}) => {
       setError(true);
       console.error(error);
     }
-  }, [userData, data]); 
+  }, [userData, data]);
 
   return (
     <>
-    <Box sx={{height: 707 , mt: '3rem', mb: '3rem' , fontFamily: 'outfit'}}>
-      <DataGrid 
-      columns={columns} 
-      rows={userData}
-      getRowId={row=> row.id}
-      pageSizeOptions={[5, 10, 20, 100]}
-      initialState={{
-        pagination: {
-          paginationModel: {
-            pageSize: 5,
-          },
-        },
-      }}
-      slots={{ toolbar: GridToolbar }}
-      slotProps={{
-        toolbar: {
-          showQuickFilter: true,
-        },
-      }} 
-      sx={
-        {
-          marginBottom: '4rem' ,
-          width: '95%',
-          bgcolor: '#212936', 
-          border : 0, 
-          '& .MuiToolbar-root , .MuiInputBase-input , .MuiDataGrid-columnHeaderTitleContainer , .MuiDataGrid-cell':{
-            color: 'white'
-          }, '& .MuiButtonBase-root .MuiSvgIcon-root ,  .MuiSvgIcon-root':{
-            color: '#2D99EF'
-          }, '& .MuiDataGrid-root , .MuiDataGrid-colCell, .MuiDataGrid-root , .MuiDataGrid-cell':{
-            maxHeight: '100px !important'
-          }, '& .MuiInputBase-root , & .MuiInputBase-input':{
-            color: '#000'
-          }, '& .MuiDataGrid-row Mui-selected , &.MuiDataGrid-row Mui-hovered':{
-            bgcolor: '#17456E'
-          }, '& .MuiDataGrid-row':{
-            height: '90px !important',
-            maxHeight: '90px !important'
-          } , '& .Mui-hovered':{
-            bgcolor: '#444654 !important'
-          }, '& .Mui-selected':{
-            bgcolor: '#17456E'
-          }, '& .MuiDataGrid-columnHeaders , & .MuiDataGrid-toolbarContainer , & .MuiDataGrid-footerContainer':{
-            height: '90px !important',
-            maxHeight: '90px !important' , 
-            fontSize: '1.2rem'
-          }, '& .MuiDataGrid-columnHeaderTitleContainer':{
-            color: '#2D99EF !important'
+      <Box sx={{ height: 707, mt: "3rem", mb: "3rem", fontFamily: "outfit" }}>
+        <DataGrid
+          columns={columns}
+          rows={userData}
+          getRowId={(row) => row.id}
+          pageSizeOptions={[5, 10, 20, 100]}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
           }}
-        }/>
-    </Box>
+          slots={{ toolbar: GridToolbar }}
+          slotProps={{
+            toolbar: {
+              showQuickFilter: true,
+            },
+          }}
+          sx={{
+            marginBottom: "4rem",
+            width: "95%",
+            bgcolor: "#212936",
+            border: 0,
+            "& .MuiToolbar-root , .MuiInputBase-input , .MuiDataGrid-columnHeaderTitleContainer , .MuiDataGrid-cell":
+              {
+                color: "white",
+              },
+            "& .MuiButtonBase-root .MuiSvgIcon-root ,  .MuiSvgIcon-root": {
+              color: "#2D99EF",
+            },
+            "& .MuiDataGrid-root , .MuiDataGrid-colCell, .MuiDataGrid-root , .MuiDataGrid-cell":
+              {
+                maxHeight: "100px !important",
+              },
+            "& .MuiInputBase-root , & .MuiInputBase-input": {
+              color: "#000",
+            },
+            "& .MuiDataGrid-row Mui-selected , &.MuiDataGrid-row Mui-hovered": {
+              bgcolor: "#17456E",
+            },
+            "& .MuiDataGrid-row": {
+              height: "90px !important",
+              maxHeight: "90px !important",
+            },
+            "& .Mui-hovered": {
+              bgcolor: "#444654 !important",
+            },
+            "& .Mui-selected": {
+              bgcolor: "#17456E",
+            },
+            "& .MuiDataGrid-columnHeaders , & .MuiDataGrid-toolbarContainer , & .MuiDataGrid-footerContainer":
+              {
+                height: "90px !important",
+                maxHeight: "90px !important",
+                fontSize: "1.2rem",
+              },
+            "& .MuiDataGrid-columnHeaderTitleContainer": {
+              color: "#2D99EF !important",
+            },
+          }}
+        />
+      </Box>
     </>
-  );  
+  );
 };
 
 export default TableComponent;
